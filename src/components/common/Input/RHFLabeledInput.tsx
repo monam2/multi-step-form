09cb3c90e484f4f624637b1b53b7085b1772622e
monam2 <kangcw0107@gmail.com>
2025-07-15 21:46:26 +0900

    refactor: 각 RHF 컴포넌트에서 error text를 RHF get 메서드로 받아오도록 수정

--- src/components/common/Input/RHFLabeledInput.tsx
+++ src/components/common/Input/RHFLabeledInput.tsx
@@ -1,4 +1,4 @@
-import { FieldValues, Path, RegisterOptions, useFormContext } from 'react-hook-form';
+import { FieldValues, get, Path, RegisterOptions, useFormContext } from 'react-hook-form';
 
 import LabeledInput from './LabeledInput';
 
@@ -20,12 +20,14 @@ const RHFLabeledInput = <T extends FieldValues>({
     register,
   } = useFormContext();
 
+  const error = get(errors, name);
+
   return (
     <LabeledInput
       label={label}
       {...restProps}
       {...register(name, rules)}
-      error={errors[name]?.message as string}
+      error={error?.message as string}
     />
   );
 };
